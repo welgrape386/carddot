fb4bfc359586ee4ec029b43bd28da67b3b22db24
naeaong <skdo8757@naver.com>
2026-04-16 14:03:18 +0900

    feat: Spring Boot 초기 세팅 및 Supabase 연결

---
+++ db/backend/src/main/java/com/carddot/carddot/CarddotApplication.java
@@ -0,0 +1,13 @@
+package com.carddot.carddot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarddotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarddotApplication.class, args);
+	}
+
+}
