ea6dc1d572c00f31994ed2fbc17cc8bbe67cdd70
naeaong <skdo8757@naver.com>
2026-06-13 16:56:55 +0900

    현대&국민 benefit_group 수정

--- intellij/src/main/java/com/carddot/carddot/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carddot.carddot.repository;
-
-import com.carddot.carddot.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-}
